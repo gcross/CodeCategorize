40788294002dfb558eaee035acb7782fcba962e2
gcross <gcross@phys.washington.edu>
2010-11-28 21:22:25 -0800

    Added lots more tests of the row-ordering algorithm.

--- tests/constraints/row_ordered.cc
+++ tests/constraints/row_ordered.cc
@@ -73,53 +73,271 @@ template<int number_of_pairs_,int number_of_constraints> struct TestClass : publ
 //@+node:gcross.20101128193219.1803: *3* 1 pair
 subSuite(_1_pair) {
 
+    //@+others
+    //@+node:gcross.20101128193219.1812: *4* 1 constraint
     subSuite(_1_constraint) {
-        createTest(in_order,1,1,1
+        createTest(_10,1,1,1
             ,1,0
         );
-        createTest(out_of_order,1,1,0
+        createTest(_01,1,1,0
             ,0,1
         );
     }
-
+    //@+node:gcross.20101128193219.1813: *4* 2 constraints
     subSuite(_2_constraints) {
-        createTest(in_order_and_in_order,1,2,1
+        createTest(_10_10,1,2,1
             ,1,0
             ,1,0
         );
-        createTest(in_order_and_tied,1,2,1
+        createTest(_10_00,1,2,1
             ,1,0
             ,0,0
         );
-        createTest(in_order_and_out_of_order,1,2,1
+        createTest(_10_01,1,2,1
             ,1,0
             ,0,1
         );
-        createTest(tied_and_in_order,1,2,1
+        createTest(_00_10,1,2,1
             ,0,0
             ,1,0
         );
-        createTest(tied_and_tied,1,2,1
+        createTest(_00_00,1,2,1
             ,0,0
             ,0,0
         );
-        createTest(tied_and_out_of_order,1,2,0
+        createTest(_00_01,1,2,0
             ,0,0
             ,0,1
         );
-        createTest(out_of_order_and_in_order,1,2,0
+        createTest(_01_10,1,2,0
             ,0,1
             ,1,0
         );
-        createTest(out_of_order_and_tied,1,2,0
+        createTest(_01_00,1,2,0
             ,0,1
             ,0,0
         );
-        createTest(out_of_order_and_out_of_order,1,2,0
+        createTest(_01_01,1,2,0
             ,0,1
             ,0,1
         );
     }
+    //@+node:gcross.20101128193219.1814: *4* 3 constraints
+    subSuite(_3_constraints) {
+        createTest(_00_00_10,1,3,1
+            ,0,0
+            ,0,0
+            ,1,0
+        );
+        createTest(_00_00_00,1,3,1
+            ,0,0
+            ,0,0
+            ,0,0
+        );
+        createTest(_00_00_01,1,3,0
+            ,0,0
+            ,0,0
+            ,0,1
+        );
+        createTest(_01_00_10,1,3,0
+            ,0,1
+            ,0,0
+            ,1,0
+        );
+    }
+    //@-others
+
+}
+//@+node:gcross.20101128193219.1815: *3* 2 pairs
+subSuite(_2_pairs) {
+
+    //@+others
+    //@+node:gcross.20101128193219.1817: *4* 1 constraint
+    subSuite(_1_constraint) {
+        createTest(_1010,2,1,1
+            ,1,0,1,0
+        );
+        createTest(_1000,2,1,1
+            ,1,0,0,0
+        );
+        createTest(_1001,2,1,0
+            ,1,0,0,1
+        );
+        createTest(_0010,2,1,0
+            ,0,0,1,0
+        );
+        createTest(_0000,2,1,1
+            ,0,0,0,0
+        );
+        createTest(_0001,2,1,0
+            ,0,0,0,1
+        );
+        createTest(_0110,2,1,0
+            ,0,1,1,0
+        );
+        createTest(_0100,2,1,0
+            ,0,1,0,0
+        );
+        createTest(_0101,2,1,0
+            ,0,1,0,1
+        );
+    }
+    //@+node:gcross.20101128193219.1819: *4* 2 constraints
+    subSuite(_2_constraints) {
+        createTest(_1010_0101,2,2,1
+            ,1,0,1,0
+            ,0,1,0,1
+        );
+        createTest(_1010_0110,2,2,0
+            ,1,0,1,0
+            ,0,1,1,0
+        );
+        createTest(_1010_1010,2,2,1
+            ,1,0,1,0
+            ,1,0,1,0
+        );
+        createTest(_1010_1001,2,2,1
+            ,1,0,1,0
+            ,1,0,0,1
+        );
+        createTest(_0000_0101,2,2,0
+            ,0,0,0,0
+            ,0,1,0,1
+        );
+        createTest(_0000_1010,2,2,1
+            ,0,0,0,0
+            ,1,0,1,0
+        );
+        createTest(_1110_0101,2,2,0
+            ,1,1,1,0
+            ,0,1,0,1
+        );
+        createTest(_1110_0001,2,2,1
+            ,1,1,1,0
+            ,0,0,0,1
+        );
+        createTest(_1110_1011,2,2,1
+            ,1,1,1,0
+            ,1,0,1,1
+        );
+    }
+    //@+node:gcross.20101128193219.1821: *4* 3 constraints
+    subSuite(_3_constraints) {
+        createTest(_1010_0101_0100,2,3,1
+            ,1,0,1,0
+            ,0,1,0,1
+            ,0,1,0,0
+        );
+        createTest(_1010_0101_0001,2,3,0
+            ,1,0,1,0
+            ,0,1,0,1
+            ,0,0,0,1
+        );
+        createTest(_1010_0101_1000,2,3,1
+            ,1,0,1,0
+            ,0,1,0,1
+            ,1,0,0,0
+        );
+        createTest(_1010_1001_0001,2,3,1
+            ,1,0,1,0
+            ,1,0,0,1
+            ,0,0,0,1
+        );
+    }
+    //@-others
+
+}
+//@+node:gcross.20101128193219.1822: *3* 3 pairs
+subSuite(_3_pairs) {
+
+    //@+others
+    //@+node:gcross.20101128193219.1824: *4* 1 constraint
+    subSuite(_1_constraint) {
+        createTest(_111000,3,1,1
+            ,1,1,1,0,0,0
+        );
+        createTest(_111100,3,1,1
+            ,1,1,1,1,0,0
+        );
+        createTest(_110010,3,1,0
+            ,1,1,0,0,1,0
+        );
+    }
+    //@+node:gcross.20101128193219.1826: *4* 2 constraints
+    subSuite(_2_constraints) {
+        createTest(_111000_011011,3,2,0
+            ,1,1,1,0,0,0
+            ,0,1,1,0,1,1
+        );
+        createTest(_101000_011011,3,2,0
+            ,1,0,1,0,0,0
+            ,0,1,1,0,1,1
+        );
+        createTest(_101000_010111,3,2,1
+            ,1,0,1,0,0,0
+            ,0,1,0,1,1,1
+        );
+        createTest(_011000_010111,3,2,0
+            ,0,1,1,0,0,0
+            ,0,1,0,1,1,1
+        );
+        createTest(_111000_010111,3,2,0
+            ,1,1,1,0,0,0
+            ,0,1,0,1,1,1
+        );
+        createTest(_111000_010101,3,2,0
+            ,1,1,1,0,0,0
+            ,0,1,0,1,0,1
+        );
+        createTest(_111000_010110,3,2,0
+            ,1,1,1,0,0,0
+            ,0,1,0,1,1,0
+        );
+        createTest(_111000_100110,3,2,1
+            ,1,1,1,0,0,0
+            ,1,0,0,1,1,0
+        );
+    }
+    //@+node:gcross.20101128193219.1828: *4* 3 constraints
+    subSuite(_3_constraints) {
+        createTest(_111000_011011_111000,3,3,0
+            ,1,1,1,0,0,0
+            ,0,1,1,0,1,1
+            ,1,1,1,0,0,0
+        );
+        createTest(_110000_011011_111000,3,3,0
+            ,1,1,0,0,0,0
+            ,0,1,1,0,1,1
+            ,1,1,1,0,0,0
+        );
+        createTest(_110000_011110_111000,3,3,0
+            ,1,1,0,0,0,0
+            ,0,1,1,1,1,0
+            ,1,1,1,0,0,0
+        );
+        createTest(_110000_101110_111000,3,3,1
+            ,1,1,0,0,0,0
+            ,1,0,1,1,1,0
+            ,1,1,1,0,0,0
+        );
+        createTest(_110000_101110_110100,3,3,0
+            ,1,1,0,0,0,0
+            ,1,0,1,1,1,0
+            ,1,1,0,1,0,0
+        );
+        createTest(_110000_111010_110100,3,3,1
+            ,1,1,0,0,0,0
+            ,1,1,1,0,1,0
+            ,1,1,0,1,0,0
+        );
+        createTest(_110000_011010_111010,3,3,0
+            ,1,1,0,0,0,0
+            ,0,1,1,0,1,0
+            ,1,1,0,0,1,0
+        );
+    }
+    //@-others
+
 }
 //@-others
 
